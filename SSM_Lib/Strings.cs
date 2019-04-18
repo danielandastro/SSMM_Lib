06d27b359731b3022640c92015102b6369d0209e
Daniel Shroff <danielshroff@hotmail.com>
2019-04-18 09:50:27 +0530

    Started writing the library

---
+++ SSM_Lib/Strings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SSM_Lib
+{
+    public class Length
+    {
+        public string GetLength(string Input)
+        {
+            byte[] temp = Encoding.ASCII.GetBytes(input);
+
+        }
+    }
+}
